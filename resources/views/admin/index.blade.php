64d202dae75bcc3f999fc5f122ed4889bc5fa62e
secretsupper <maulanadityaaa@gmail.com>
2022-01-04 03:57:13 +0700

    bismillah skripsi

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h1>ADMIN DASHBOARD</h1>
+        </div>
+    </div>
+@endsection
