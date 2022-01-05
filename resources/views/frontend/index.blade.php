ac2ff17a80ba810a342ace562d60d47ce553f1c0
secretsupper <maulanadityaaa@gmail.com>
2022-01-06 03:10:55 +0700

    add slider and navbar for user

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.front')
+
+@section('title')
+    Selamat Datang di Toko Sandal
+@endsection
+
+@section('content')
+    @include('layouts.inc.slider')
+@endsection
---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>
+        @yield('title')
+    </title>
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    @include('layouts.inc.frontnavbar')
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    @if (session('status'))
+        <script>
+            swal("Pemberitahuan", "{{ session('status') }}", "success");
+        </script>
+    @endif
+
+    @yield('scripts')
+</body>
+
+</html>
